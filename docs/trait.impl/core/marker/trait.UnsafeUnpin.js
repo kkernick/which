d081c9385df44beac4c1d9c176192f095f3692f0
kkernick <90726554+kkernick@users.noreply.github.com>
2026-08-28 18:43:27 -0600

    Update depends+docs

--- docs/trait.impl/core/marker/trait.UnsafeUnpin.js
+++ docs/trait.impl/core/marker/trait.UnsafeUnpin.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["which",[["impl UnsafeUnpin for <a class=\"enum\" href=\"which/enum.Error.html\" title=\"enum which::Error\">Error</a>",0,1,["which::Error"]]]]]);
+    const implementors = Object.fromEntries([["which",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.UnsafeUnpin.html\" title=\"trait core::marker::UnsafeUnpin\">UnsafeUnpin</a> for <a class=\"enum\" href=\"which/enum.Error.html\" title=\"enum which::Error\">Error</a>",0,1,["which::Error"]]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[144]}
+//{"start":59,"fragment_lengths":[287]}
